e30092f5d8790a9ffcf458cc4a291a4a3c894478
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-09-09 01:01:13 -0400

    rename package

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from csh_uuid import app
-from csh_uuid.ldap import ldap_init
+from csh_uuid_http import app
+from csh_uuid_http.ldap import ldap_init
 
 app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
 ldap_init(app)
