373d2493e2c005324551d98ad729844486c1366a
Liam Middlebrook <liammiddlebrook@gmail.com>
2016-10-02 23:05:58 -0400

    convert to use csh_ldap

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from csh_uuid_http import app
-from csh_uuid_http.ldap import ldap_init
+
+from csh_ldap import CSHLDAP
 
 app.config.from_pyfile(os.path.join(os.getcwd(), "config.py"))
-ldap_init(app)
-
+app.config['LDAP_CONN'] = CSHLDAP(
+                            app.config['LDAP_BIND_DN'],
+                            app.config['LDAP_BIND_PW'])
 if __name__ == '__main__':
     app.run(host=app.config['IP'], port=app.config['PORT'])
 
--- csh_uuid_http/__init__.py
+++ csh_uuid_http/__init__.py
@@ -6,10 +6,6 @@ from uuid import NAMESPACE_OID
 
 from flask import Flask
 
-from csh_uuid.ldap import ldap_init
-from csh_uuid.ldap import get_uid
-from csh_uuid.ldap import get_uuid
-
 app = Flask(__name__)
 
 word_site = "http://svnweb.freebsd.org/" \
@@ -21,17 +17,21 @@ WORDS = response.content.splitlines()
 
 @app.route('/uuid/<uid>')
 def uid_to_uuid(uid):
-    uuid = get_uuid(app, uid)
-
-    if uuid is None:
+    try:
+        member = app.config['LDAP_CONN'].get_member(uid, True)
+        uuid = member.entryUUID
+    except KeyError:
         return str(uuid5(NAMESPACE_OID, uid))
+
     return uuid
 
 
 @app.route('/uid/<uuid>')
 def uuid_to_uid(uuid):
-    uid = get_uid(app, uuid)
-
-    if uid is None:
+    try:
+        member = app.config['LDAP_CONN'].get_member(uuid, False)
+        uid = member.uid
+    except KeyError:
         return random.choice(WORDS).lower()
+
     return uid
